5db6b710766c27ecdf153324c14cdd1c584dfcd9
MR-5OBOT <yassir7m7@gmail.com>
2025-05-12 11:48:30 +0100

    .

--- expirements.py
+++ expirements.py
@@ -5,13 +5,27 @@ import seaborn as sns
 
 from helpers.utils import *
 from helpers.visualizations import *
-from helpers.formulas import *
 from helpers.data_preprocessing import *
+from helpers.data_cleaning import *
 
 
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQL7L-HMzezpuFCDOuS0wdUm81zbX4iVOokaFUGonVR1XkhS6CeDl1gHUrW4U0Le4zihfpqSDphTu4I/pub?gid=212787870&single=true&output=csv"
-df = pd.read_csv("./data/TV_paper_test_data.csv")
+# df = pd.read_csv("./data/TV_paper_test_data.csv")
+df = pd.read_csv(url)
 
-s = df["Time"]
-print(datetime_to_time(s))
+# pl = pl_raw(df)
+# print(pl_raw)
+# print(datetime_to_time(s))
 # print(df.head())
+
+test_inputs = df["pl_by_percentage"]
+
+# Convert to a pandas Series
+series = pd.Series(test_inputs)
+
+# Clean the series
+cleaned_series = clean_numeric_series(series)
+
+# Print the cleaned series
+print(cleaned_series)
+
